123ab76372563306ef7f746122cb809f070c2401
Vicent Llopico <70440969+vllopico@users.noreply.github.com>
2021-12-10 17:25:04 +0100

    Update vector.rs

--- vector.rs
+++ vector.rs
@@ -7,12 +7,13 @@ fn suma_vec(v:Vec<i32>) -> i32 {
 }
 
 fn main() {
-  let sv:i32 = suma_vec(v);
   let mut v = Vec::new();
   v.push(1);
   v.push(2);
   v.push(3);
   v.push(4);
   v.push(5);
+  
+  let sv:i32 = suma_vec(v);
   println!("Suma elements vector -> {}",sv);
 }
